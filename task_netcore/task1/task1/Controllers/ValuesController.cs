752bc8a3501b64a5893df1519f720831d865b7ed
alekslevko <alekslevko@mail.ru>
2018-08-09 14:36:22 +0300

    completed task1

---
+++ task_netcore/task1/task1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using task1.Models;
+
+namespace task1.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        [HttpGet]
+        public ActionResult Get(SumModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                return Ok(new SumModel { A = model.A, B = model.B, Sum = (int)(model.A + model.B) });
+            }
+            else
+            {
+                return BadRequest(ModelState);
+            }
+            
+        }        
+    }
+}
---
+++ task_netcore/task1/task1/Models/SumModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace task1.Models
+{
+    public class SumModel
+    {
+        [Required(ErrorMessage = "Параметр обязательный")]
+        [Range(1, Double.MaxValue, ErrorMessage = "Параметр должен быть положительным")]
+        public int? A { get; set; }
+
+        [Required(ErrorMessage = "Параметр обязательный")]
+        [Range(Double.MinValue, -1, ErrorMessage ="Параметр должен быть отрицательным")]
+        public int? B { get; set; }
+
+        public int Sum { get; set; }
+    }
+}
